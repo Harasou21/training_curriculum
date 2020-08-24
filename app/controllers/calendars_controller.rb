6349ed12714ee8fb788fc97023d285e6973d0afb
Harasou21 <67535886+Harasou21@users.noreply.github.com>
2020-08-24 22:13:32 +0900

    get_Weekをget_weekへ変更

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,7 +2,7 @@ class CalendarsController < ApplicationController
 
 
   def index
-    get_Week
+    get_week
     @plan = Plan.new
   end
 
@@ -18,7 +18,7 @@ class CalendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
-  def get_Week
+  def get_week
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
     
     @todays_date = Date.today
